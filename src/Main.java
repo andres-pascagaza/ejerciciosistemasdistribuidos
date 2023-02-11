0cfb7b03ed8da5835546e448d06250b4a9a99cc0
zip-b205-e-005 <zip-b205-e-005@UMD.LOCAL.EDU>
2023-02-10 20:24:33 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int a = 5, b = 6;
+        System.out.println("a + b");
+        System.out.println("Hello world!");
+
+        double c = 2.14, d = 5.14;
+
+        System.out.println("a + b");
+
+
+        System.out.println("Hello world!");
+
+
+    }
+}
